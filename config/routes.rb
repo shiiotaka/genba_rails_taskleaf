a39796274dfda75dee4560bb1b54b237cf526b15
shiiotaka <shiiotaka@gmail.com>
2021-01-10 23:05:24 +0900

    admin_user 2021/1/10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
   root 'tasks#index'
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
