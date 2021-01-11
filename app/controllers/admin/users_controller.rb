05b8a20e3696daa0f9618507357d329f9812caa0
shiiotaka <shiiotaka@gmail.com>
2021-01-11 20:40:14 +0900

    finish ch04 2021/1/11

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,21 @@
 class Admin::UsersController < ApplicationController
 
+  before_action :require_admin
+
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -17,14 +28,28 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  def show
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー#{@user.name}を更新しました。"
+    else
+      render 'edit'
+    end
   end
 
-  def edit
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー#{@user.name}を削除しました"
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_url unless current_user.admin?
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :name, presence: true, length: {maximum: 30}
   validate :validate_name_not_including_comma
+  scope :recent, -> { order(created_at: :desc) }
 
   private
   def validate_name_not_including_comma
